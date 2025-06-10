757d25f6d22d05c511e127618ed56032d005f3c6
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2025-06-10 13:19:05 +0500

    fix:ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { EventLogger } from './core/EventLogger';
 export { ValidationError } from './utils/validation';
 
-export type { Environment, LogLevel, LogData, TelegramProviderConfig, EventLoggerConfig } from './core/types';
+export type { Environment, LogLevel, TelegramProviderConfig, EventLoggerConfig } from './core/types';
 
 export type { IEventLogger, IProvider, ITelegramProvider } from './core/interfaces';
 
