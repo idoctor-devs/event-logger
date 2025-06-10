223fb87fdeab7aba08f910380413b6071222d661
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2025-06-10 13:51:33 +0500

    feat: добавил метод форматирования даты в TelegramProvider

--- src/providers/telegram/TelegramProvider.ts
+++ src/providers/telegram/TelegramProvider.ts
@@ -36,8 +36,22 @@ export class TelegramProvider extends BaseProvider implements ITelegramProvider 
     return this.config.chatId;
   }
 
+  private formatDate(date: Date | string = new Date()) {
+    const data = typeof date === 'string' ? new Date(date) : date;
+
+    const day = String(data.getDate()).padStart(2, '0');
+    const month = String(data.getMonth() + 1).padStart(2, '0');
+    const year = data.getFullYear();
+
+    const hours = String(data.getHours()).padStart(2, '0');
+    const minutes = String(data.getMinutes()).padStart(2, '0');
+    const seconds = String(data.getSeconds()).padStart(2, '0');
+
+    return `${day}.${month}.${year} ${hours}.${minutes}.${seconds}`;
+  }
+
   private formatMessage(message: string, level: LogLevel, metadata?: Record<string, string | number>): string {
-    const timestamp = new Date().toISOString();
+    const timestamp = this.formatDate();
     const levelEmoji = this.getLevelEmoji(level);
 
     let formattedMessage = `${levelEmoji} [${level.toUpperCase()}] ${timestamp}`;
